7c20bcbfbb7cfbb2e847e1b09a79c702058a4864
agusssssss <hairismahoree@gmail.com>
2024-06-24 17:55:37 +0800

    update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,18 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Package;
-use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index() : View {
         $packages = Package::all();
-        $googleDriveApi = "https://script.google.com/macros/s/AKfycbzHCSYetUYwf5rH6i5wc9-Z-AwF2b9a5iJ-FjtvQFpagmpEcwk1BKKt-3fI4W5waaAo/exec";
-
-        $googleDriveFilesResponse = Http::get($googleDriveApi);
-        $googleDriveFiles = $googleDriveFilesResponse->json();
-
-        return view("dashboard.index", compact("packages", "googleDriveFiles"));
+        return view("dashboard.index", compact("packages"));
     }
 }
