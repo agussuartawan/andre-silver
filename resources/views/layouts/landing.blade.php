86f42fe4aec51ce64b1104a566d6ff993278a9c8
agusssssss <hairismahoree@gmail.com>
2024-06-24 18:03:15 +0800

    add favicon

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,6 +6,11 @@
 <meta content="Silver Class" name="keywords">
 <meta content="Make your own silver by yourself" name="description">
 
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+<link rel="manifest" href="/favicon/site.webmanifest">
+
 <title>{{ config('app.name', 'Andre Silver Class') }}</title>
 
 <!-- Fonts -->
